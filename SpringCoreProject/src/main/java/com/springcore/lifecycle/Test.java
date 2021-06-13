0f3d48626a408c16369de144d5593ef29fca0228
bkp3 <bholakumar.7372@gmail.com>
2021-06-13 15:21:46 +0530

    implementing bean life cycle using interface

--- SpringCoreProject/src/main/java/com/springcore/lifecycle/Test.java
+++ SpringCoreProject/src/main/java/com/springcore/lifecycle/Test.java
@@ -10,11 +10,17 @@ public class Test {
 		// TODO Auto-generated method stub
 
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/lifecycle/config.xml");
-		Samosa s1 = (Samosa) context.getBean("s1");
-		System.out.println(s1);
 
 		// registering shutdown hook
 		context.registerShutdownHook();
+
+		Samosa s1 = (Samosa) context.getBean("s1");
+		System.out.println(s1);
+
+		System.out.println("-------------------------------------------------------");
+		
+		Pepsi p1 = (Pepsi)context.getBean("p1");
+		System.out.println(p1);
 	}
 
 }
