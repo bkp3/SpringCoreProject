7eb0a782cd55cb6fb8fa68df3f0dfd81e1fac7f0
bkp3 <bholakumar.7372@gmail.com>
2021-06-14 12:36:58 +0530

    @Autowired annotation for autowiring

---
+++ SpringCoreProject/src/main/java/com/springcore/autowiring/annotation/Emp.java
@@ -0,0 +1,37 @@
+package com.springcore.autowiring.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Emp {
+	
+	@Autowired
+	private Address address;
+
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Emp(Address address) {
+		super();
+		this.address = address;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [address=" + address + "]";
+	}
+
+	
+	
+	
+
+}
