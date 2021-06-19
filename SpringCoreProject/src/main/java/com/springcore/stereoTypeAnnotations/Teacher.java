08210b90a9439e1e932df9e31555a49f4f1d84fc
bkp3 <bholakumar.7372@gmail.com>
2021-06-19 21:57:21 +0530

    spring bean scope (singleton, prototype)

---
+++ SpringCoreProject/src/main/java/com/springcore/stereoTypeAnnotations/Teacher.java
@@ -0,0 +1,22 @@
+package com.springcore.stereoTypeAnnotations;
+
+public class Teacher {
+	
+	private String subject;
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	@Override
+	public String toString() {
+		return "Teacher [subject=" + subject + "]";
+	}
+	
+	
+
+}
--- SpringCoreProject/src/main/java/com/springcore/stereoTypeAnnotations/Test.java
+++ SpringCoreProject/src/main/java/com/springcore/stereoTypeAnnotations/Test.java
@@ -7,9 +7,20 @@ public class Test {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/stereoTypeAnnotations/stereoConfig.xml");
+		
 		Student st=context.getBean("student",Student.class);
-		System.out.println(st);;
+		System.out.println(st.hashCode());
 
+		Student st1=context.getBean("student",Student.class);
+		System.out.println(st1.hashCode());
+		
+		Teacher tea=context.getBean("teacher",Teacher.class);
+		System.out.println(tea);
+		System.out.println(tea.hashCode());
+		
+		Teacher tea1=context.getBean("teacher",Teacher.class);
+		System.out.println(tea1);
+		System.out.println(tea1.hashCode());
 	}
 
 }
