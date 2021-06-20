49780ba351ec9a082aaf84cd270e5833c121a49f
bkp3 <bholakumar.7372@gmail.com>
2021-06-20 09:46:26 +0530

    how to invoke static method , variables and creating object

--- SpringCoreProject/src/main/java/com/springcore/spel/Demo.java
+++ SpringCoreProject/src/main/java/com/springcore/spel/Demo.java
@@ -11,7 +11,17 @@ public class Demo {
 	
 	@Value("#{1+3+4+5+87}")
 	private int y;
+	
+	@Value("#{ T(java.lang.Math).sqrt(25) }")
+	private double z;
+	
 
+	@Value("#{ T(java.lang.Math).E }")
+	private double e;
+
+	@Value("#{ new java.lang.String('Ankit Kumar') }")
+	private String name;
+	
 	public int getX() {
 		return x;
 	}
@@ -28,9 +38,40 @@ public class Demo {
 		this.y = y;
 	}
 
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+	
+	
+
+	public double getE() {
+		return e;
+	}
+
+	public void setE(double e) {
+		this.e = e;
+	}
+	
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", name=" + name + "]";
 	}
+
+	
+	
 
 }
